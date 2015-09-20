eb0777807efd18c83e8e758be48e1f1875b4daef
frederic-lepoutre <frederic.lepoutre@outlook.com>
2015-09-20 18:52:17 +0000

    Update Configuration_Path.py

--- Configuration_Path.py
+++ Configuration_Path.py
@@ -1,4 +1,17 @@
 """Configure le poste de travail de manière à ce qu'il puisse directement utiliser les modules"""
+
 import sys
 sys.path.append(currentPath)
 from modules import *
+
+import os
+ 
+dossier = os.path.dirname(os.path.abspath(__file__))
+ 
+while not dossier.endswith('modules'):
+    dossier = os.path.dirname(dossier)
+ 
+dossier = os.path.dirname(dossier)
+ 
+if dossier not in sys.path:
+    sys.path.append(dossier)
